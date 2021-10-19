9a3fd6630a010054130d233c2108001636d1955e
Nanda Amanta <nansamgr@gmail.com>
2021-10-19 22:40:50 +0800

    add search page

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:suka_nonton/presentation/AppBar/bottom_app_bar.dart';
 import 'package:suka_nonton/presentation/home/components/recent.dart';
 import 'package:suka_nonton/presentation/home/components/recomendation.dart';
+import 'package:suka_nonton/presentation/search/search_sreen.dart';
 import 'package:suka_nonton/value/colors.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,18 +14,17 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int selectedIndex = 0;
+  PageController pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: color3,
+        backgroundColor: color3,
         appBar: AppBar(
           backgroundColor: color1,
           actions: [
             IconButton(
-                onPressed: () {
-                  
-                },
+                onPressed: () {},
                 icon: Icon(
                   Icons.star,
                   color: Colors.amber,
@@ -33,42 +32,100 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
           title: Text("Suka Nonton"),
         ),
-        body: ListView(
+        body: PageView(
+          onPageChanged: (inx){
+            print(inx);
+            setState(() {
+              
+              selectedIndex = inx;
+            });
+          },
           children: [
-            Container(
-              margin: EdgeInsets.only(top: 20, left: 10),
-              child: Stack(
-                  children: <Widget>[
-                    // Stroked text as border.
-                    Text(
-                      'Recommendation',
-                      style: TextStyle(
-                        letterSpacing: 2,
-                        fontSize: 20,
-                        foreground: Paint()
-                          ..style = PaintingStyle.stroke
-                          ..strokeWidth = 2
-                          ..color = Colors.black,
-                      ),
-                    ),
-                    // Solid text as fill.
-                    Text(
-                      'Recommendation',
-                      style: TextStyle(
-                        letterSpacing: 3,
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                )
-              ),
-            
-            SizedBox(height: 10,),
-            RecomendationWidget(),
-            Recent()
+            ListView(
+              children: [
+                Container(
+                    margin: EdgeInsets.only(top: 20, left: 10),
+                    child: Stack(
+                      children: <Widget>[
+                        // Stroked text as border.
+                        Text(
+                          'Recommendation',
+                          style: TextStyle(
+                            letterSpacing: 2,
+                            fontSize: 20,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 2
+                              ..color = Colors.black,
+                          ),
+                        ),
+                        // Solid text as fill.
+                        Text(
+                          'Recommendation',
+                          style: TextStyle(
+                            letterSpacing: 3,
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                RecomendationWidget(),
+                Recent()
+              ],
+            ),
+            SearchScreen(),
           ],
+          controller: pageController,
         ),
-        bottomNavigationBar: BottomAppBarWidget(selectedIndex: selectedIndex,));
+        bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.blueGrey,
+            unselectedIconTheme: IconThemeData(color: Colors.blueGrey),
+            selectedIconTheme: IconThemeData(color: Colors.white),
+            type: BottomNavigationBarType.fixed,
+            elevation: 5,
+            items: [
+              BottomNavigationBarItem(
+                label: "Home",
+                icon: Icon(
+                  Icons.home,
+                  size: 25,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "Search",
+                icon: Icon(
+                  Icons.search,
+                  size: 25,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "Favorite",
+                icon: Icon(
+                  Icons.favorite_border,
+                  size: 25,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "Profile",
+                icon: Icon(
+                  Icons.person_pin,
+                  size: 25,
+                ),
+              ),
+            ],
+            backgroundColor: color1,
+            currentIndex: selectedIndex,
+            onTap: (int index) {
+              setState(() {
+                print(index);
+                selectedIndex = index;
+                pageController.jumpToPage(index);
+              });
+            }));
   }
 }
--- lib/presentation/search/search_sreen.dart
+++ lib/presentation/search/search_sreen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:suka_nonton/presentation/AppBar/bottom_app_bar.dart';
+import 'package:suka_nonton/presentation/home/components/recent.dart';
 
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({ Key? key }) : super(key: key);
+  const SearchScreen({Key? key}) : super(key: key);
 
   @override
   _SearchScreenState createState() => _SearchScreenState();
@@ -11,8 +11,28 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomAppBarWidget(selectedIndex: 0,),
+    return Container(
+      padding: EdgeInsets.only(left: 20,right: 20,top: 20),
+      child: Column(
+        children: [
+          Container(
+            height: 60,
+            decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)),color: Colors.white,),
+            
+            child: TextField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Search Your Movie ...' ),
+                  
+                  
+            ),
+          ),
+          SizedBox(height: 50,),
+          Expanded(
+            
+            child: Recent(),
+          )
+        ],
+      ),
     );
   }
 }
